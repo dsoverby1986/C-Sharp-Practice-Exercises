927eb9b55f7f132de89071af357b13a10cab72d3
shaneoverby <soverby@core-techs.net>
2017-06-21 14:23:25 -0400

    Changed the 2DStringArrayExtensions.GetRow() method to a generic method and refactored it into a generics 2D array extensions class

---
+++ C Sharp Practice Exercises/Extensions/2DArrayExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice_Exercises.Extensions
+{
+    static class _2DArrayExtensions
+    {
+        public static T[] GetRow<T>(this T[,] _2dArray, int row)
+        {
+            int cols = _2dArray.GetUpperBound(1) + 1;
+            T[] array = new T[cols];
+            for (int i = 0; i < _2dArray.GetLength(0); i++)
+                if (i == row)
+                    for (int j = 0; j < cols; j++)
+                        array[j] = _2dArray[i, j];
+
+            return array;
+        }
+    }
+}
--- C Sharp Practice Exercises/Extensions/2DStringArrayExtensions.cs
+++ C Sharp Practice Exercises/Extensions/2DStringArrayExtensions.cs
@@ -22,17 +22,5 @@ namespace C_Sharp_Practice_Exercises.Extensions
 
             return length;
         }
-
-        public static string[] GetRow(this string[,] _2dArray, int row)
-        {
-            int cols = _2dArray.GetUpperBound(1) + 1;
-            string[] array = new string[cols];
-            for(int i = 0; i < _2dArray.GetLength(0); i++)
-                if(i == row)
-                    for(int j = 0; j < cols; j++)
-                        array[j] = _2dArray[i, j];
-
-            return array;
-        }
     }
 }
